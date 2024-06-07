86fce56a51c9894723b6de6f695823b2cd72f48d
ThevinduKevin <thevindukevin@gmail.com>
2024-06-07 17:22:16 +0530

    Connect to database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 
@@ -8,7 +9,7 @@ const workoutRoutes = require('./routes/workouts')
 const app = express()
 
 //middleware
-app.use(express.json(n))
+app.use(express.json())
 
 app.use((req,res,next) => {
     console.log(req.path, req.method)
@@ -18,8 +19,17 @@ app.use((req,res,next) => {
 //routes
 app.use('/api/workouts', workoutRoutes)
 
-//listen for request
-app.listen(process.env.PORT, () => {
-    console.log('listening on port ', process.env.PORT)
-})
+//connect to bd
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        //listen for request
+        app.listen(process.env.PORT, () => {
+            console.log('listening on port ', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+
 
